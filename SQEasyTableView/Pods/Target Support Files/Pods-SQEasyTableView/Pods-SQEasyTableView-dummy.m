2633244245d171556c34a3e4ad567729d749e80b
sunqi <qi.sun@baidao.com>
2018-07-20 11:54:20 +0800

    初始化项目

---
+++ SQEasyTableView/Pods/Target Support Files/Pods-SQEasyTableView/Pods-SQEasyTableView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SQEasyTableView : NSObject
+@end
+@implementation PodsDummy_Pods_SQEasyTableView
+@end
---
+++ SQEasyTableView/SQEasyTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQEasyTableView
+//
+//  Created by sunqi on 2018/7/20.
+//  Copyright © 2018年 sunqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
